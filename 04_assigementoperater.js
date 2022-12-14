40992b155fe62b963421be2b9e9470248885b3fb
akashkambale21 <119153881+akashkambale21@users.noreply.github.com>
2022-12-14 09:32:46 +0530

    Add files via upload

---
+++ 04_assigementoperater.js
@@ -0,0 +1,13 @@
+console.log(`============ Assignment Operators =================`);
+var modulusResult = num1 % 3;
+console.log(`Modulus is ${modulusResult}`);
+
+var num1 = 10;
+var num2 = 5;
+num1 += num2; // num1 = num1 + num2
+              // num1 = 15;
+console.log(`Compound Addition ${num1}`);
+
+var num1 = 10;
+var num2 = 5;
+console.log(`Compound Subtraction ${num1-=num2}`); // num1 = 15 - 5
