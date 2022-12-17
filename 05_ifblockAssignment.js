7d946830ffb76f6de49f225b0300c7ccc345e671
akashkambale21 <119153881+akashkambale21@users.noreply.github.com>
2022-12-17 20:08:09 +0530

    if block assignment 1
    if block assignment 1

---
+++ 05_ifblockAssignment.js
@@ -0,0 +1,98 @@
+console.log(`############## Q1 ##############`);
+console.log(``);
+var age=function(voteages){
+    if (voteages>=18);
+   {console.log(` your Age=${voteages} is eligible for vote`);}
+}
+
+age(45);
+var age1=function(voteagess){
+    if (voteagess>=18);
+   {console.log(` your Age=${voteagess} is not eligible for vote`);}
+}
+
+age1(17)
+var age=function(voteages){
+    if (voteages>=18);
+   {console.log(` your Age=${voteages} is not eligible for vote`);}
+}
+
+age(8)
+var age=function(voteages){
+    if (voteages>=18);
+   {console.log(` your Age=${voteages} is eligible for vote`);}
+}
+
+age(20)
+var age=function(voteages){
+    if (voteages>=18);
+   {console.log(` your Age=${voteages} is Invalid data `);}
+}
+
+age(-10)
+var age=function(voteages){
+    if (voteages>=18);
+   {console.log(` your Age=${voteages} is Invalid data `);}
+}
+
+age(200)
+var age=function(voteages){
+    if (voteages>=18);
+   {console.log(` your Age=${voteages} is Invalid data `);}
+}
+
+age(0);
+console.log(``);
+
+console.log(`############## Q2 ##############`);
+console.log(``);
+
+function mark (grademark){
+    if (grademark>=90) {
+        console.log(`funtastic mark:${grademark} , your grade is A+`);
+    }
+}
+mark(98)
+function mark1 (grademark){
+    if (grademark>=75) {
+        console.log(`Excellent mark:${grademark} , your grade is A`);
+    }
+}
+mark1(80)
+function mark (grademark){
+    if (grademark>=90) {
+        console.log(`funtastic mark:${grademark} , your grade is A+`);
+    }
+}
+mark(90)
+function mark2 (grademark){
+    if (grademark<=90) {
+        console.log(` mark:${grademark} , please provide valid mark`);
+    }
+}
+mark2(0)
+function mark3 (grademark){
+    if (grademark>=90) {
+        console.log(` mark:${grademark} , please provide valid mark`);
+    }
+}
+mark3(150)
+function mark4 (grademark){
+    if (grademark<=90) {
+        console.log(` mark:${grademark} ,your grade is c, need improvement`);
+    }
+}
+mark4(35)
+function mark5 (grademark){
+    if (grademark<=90) {
+        console.log(` mark:${grademark} , please provide valid mark`);
+    }
+}
+mark5(-7)
+
+function mark7 (grademark){
+    if (grademark<=90) {
+        console.log(`mark:${grademark} ,your grade is c, need improvement`);
+    }
+}
+mark7(49)
